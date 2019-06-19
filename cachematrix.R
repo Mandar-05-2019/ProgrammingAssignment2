aeba3c8ef0132a9c654f3b617cf7fab5edef0ba8
Mandar-05-2019 <mandar.joshi07@gmail.com>
2019-06-19 13:55:04 +0530

    Assignment submission - 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create cache for sampl calculations of invers of matrices:
+## Assumption : input matrices are invertible
 
-## Write a short comment describing this function
+## Create a soecical "matrix" which cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of matrix and stores in cache if not.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                #message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
